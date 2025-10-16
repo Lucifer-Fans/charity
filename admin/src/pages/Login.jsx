4c95b5f671cfe583d963e8d311653bbb1c91d9b2
Lucifer-Fans <only4rizvi@gmail.com>
2025-10-16 11:31:59 +0530

    Update Login.jsx

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -102,7 +102,7 @@ const Login = () => {
     setError("");
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://thecresent-backend.onrender.com/api/auth/login", {
         username,
         password,
       });
